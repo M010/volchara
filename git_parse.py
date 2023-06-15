e5a4e965ed1b4eb418dba17d7dace421bb76379b
Гребнев Никита <NAlGrebnev@sberbank.ru>
2023-06-15 21:19:50 +0300

    git_parse fix

--- git_parse.py
+++ git_parse.py
@@ -2,6 +2,9 @@ import git
 from files_with_score import *
 from datetime import datetime
 from pathlib import Path
+import os
+
+CPP_EXT = (".h", ".cpp", ".hpp", ".cxx", ".hxx", ".cc", ".c")
 
 
 class RepoParser:
@@ -23,15 +26,20 @@ class RepoParser:
 
     def process_files(self) -> Files:
         files_to_process = []
-        for (dir_path, dir_names, file_names) in walk(repo_path):
+        for (dir_path, dir_names, file_names) in os.walk(self.repo_path):
             if ".git" in dir_names:
                 dir_names.remove(".git")
             for file_name in file_names:
+                if not Path(file_name).suffix in CPP_EXT:
+                    continue
                 full_path = Path(dir_path, file_name)
-                files_to_process.insert(full_path)
+                files_to_process.append(str(full_path.absolute()))
 
         files_with_score = Files(files=[])
         for file in files_to_process:
             res = self.process_file(file)
             files_with_score.files.append(res)
         return files_with_score
+
+parser = RepoParser(Path("."))
+files = parser.process_files()
