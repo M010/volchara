b5adc717a64aa532b94e1f4abf2a83c2150e8c6a
Aleksei Ponyatov <ponyatov94@gmail.com>
2023-06-15 21:04:24 +0300

    change foo

--- example/foo.cpp
+++ example/foo.cpp
@@ -2,27 +2,19 @@
 #include <iostream>
 
 void foo() {
-        for (int num = 1; num <= 100; num += 1)
-    {
-        if ((num % 3 == 0) && (num % 5 == 0))
-        {
-            std::cout << "FizzBuzz" << std::endl;
-        }
-        else if (num % 3 == 0)
-        {
-            std::cout << "Fizz" << std::endl;
-        }
-        else if (num % 5 == 0)
-        {
-            std::cout << "Buzz" << std::endl;
-        }
-        else if (num % 75 == 0)
-        {
-            std::cout << "Fizz" << std::endl;
-        }
-        else
-        {
-            std::cout << num << std::endl;
-        }
-    }
+    std::cout << "Одинокий волк одинок когда он волк и одинок" << std::endl;
+    std::cout << "Одинокий волк одинок когда он волк и одинок" << std::endl;
+    std::cout << "Одинокий волк одинок когда он волк и одинок" << std::endl;
+    std::cout << "Одинокий волк одинок когда он волк и одинок" << std::endl;
+    std::cout << "Одинокий волк одинок когда он волк и одинок" << std::endl;
+    std::cout << "Одинокий волк одинок когда он волк и одинок" << std::endl;
+    std::cout << "Одинокий волк одинок когда он волк и одинок" << std::endl;
+    std::cout << "Одинокий волк одинок когда он волк и одинок" << std::endl;
+    std::cout << "Одинокий волк одинок когда он волк и одинок" << std::endl;
+    std::cout << "Одинокий волк одинок когда он волк и одинок" << std::endl;
+    std::cout << "Одинокий волк одинок когда он волк и одинок" << std::endl;
+    std::cout << "Одинокий волк одинок когда он волк и одинок" << std::endl;
+    std::cout << "Одинокий волк одинок когда он волк и одинок" << std::endl;
+    std::cout << "Одинокий волк одинок когда он волк и одинок" << std::endl;
+
 }
